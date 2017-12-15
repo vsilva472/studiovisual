f2ce0b631f4979d067e9c9765f6d4be6f26724bb
Vinicius Silva <vsilva472@gmail.com>
2017-12-15 14:34:00 -0200

    finalizado questao 1 preposicoes no texto b

--- index.php
+++ index.php
@@ -9,11 +9,37 @@
 require_once "vendor/autoload.php";
 
 use StudioVisual\App\Readers\TxtReader;
+use StudioVisual\App\Dialetos\Booglan\Booglan;
 
-$filepath = 'config/a.txt';
+$filepath = 'config/b.txt';
 $TxtReader = new TxtReader( $filepath );
 
 $data = trim($TxtReader->getData());
 
-echo $data;
+$Booglan = new Booglan( 'twhzkdfvcjxlrnqmgpsb' );
+$prepositions_count = $Booglan->getPropositionCountFrom($data);
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ol style="list-style: none;">
+        <li>
+            <h3>Quantas preposições existem no texto B?</h3>
+            <p>R: <?php print $prepositions_count; ?></p>
+        </li>
+    </ol>
+</body>
+</html>
+
+
 
